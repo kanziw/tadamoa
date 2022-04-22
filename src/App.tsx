4b252d2716e5eca7761e9cd8a1654eb0764dc4b5
kanziw <kanziwoong@gmail.com>
2022-04-22 16:01:05 +0900

    Fix typos

--- src/App.tsx
+++ src/App.tsx
@@ -64,32 +64,11 @@ const App = ({ closeMini, isOnNativeWebview }: Props) => {
   }, [])
 
   const fire = useCallback(() => {
-    makeShot(0.25, {
-      spread: 26,
-      startVelocity: 55
-    })
-
-    makeShot(0.2, {
-      spread: 60
-    })
-
-    makeShot(0.35, {
-      spread: 100,
-      decay: 0.91,
-      scalar: 0.8
-    })
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 25,
-      decay: 0.92,
-      scalar: 1.2
-    })
-
-    makeShot(0.1, {
-      spread: 120,
-      startVelocity: 45
-    })
+    makeShot(0.25, { spread: 26, startVelocity: 55 })
+    makeShot(0.2, { spread: 60 })
+    makeShot(0.35, { spread: 100, decay: 0.91, scalar: 0.8 })
+    makeShot(0.1, { spread: 120, startVelocity: 25, decay: 0.92, scalar: 1.2 })
+    makeShot(0.1, { spread: 120, startVelocity: 45 })
 
   }, [makeShot])
 
@@ -108,7 +87,7 @@ const App = ({ closeMini, isOnNativeWebview }: Props) => {
   }, [intervalId, nextTickAnimation])
 
   const pauseAnimation = useCallback(() => {
-    if (intervalId){
+    if (intervalId) {
       clearInterval(intervalId)
     }
     setIntervalId(null)
